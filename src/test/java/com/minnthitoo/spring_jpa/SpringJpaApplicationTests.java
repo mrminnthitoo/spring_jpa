c56986937afb31e0f8d29f17a23e0aea77d7e445
minnthitoo <mto.forlearning@gmail.com>
2024-09-26 12:18:19 +0630

    init

---
+++ src/test/java/com/minnthitoo/spring_jpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minnthitoo.spring_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
