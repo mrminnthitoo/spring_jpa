f7134819044df7b56ec94eceddb9c2998863ec87
mrminnthitoo <mrminnthitoo@gmail.com>
2024-11-21 22:24:17 +0630

    Query By Example

---
+++ src/test/java/com/minnthitoo/spring_jpa/repository/MovieQBETest.java
@@ -0,0 +1,37 @@
+package com.minnthitoo.spring_jpa.repository;
+
+import com.minnthitoo.spring_jpa.model.entity.Movie;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+
+import java.util.List;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MovieQBETest {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Transactional
+    @Test
+    public void testMovieQBE(){
+        Movie movie = new Movie();
+        movie.setGenre("Drama");
+
+        ExampleMatcher matcher = ExampleMatcher.matching();
+//                .withIgnorePaths("genre");
+        Example<Movie> example = Example.of(movie, matcher);
+        List<Movie> movies = this.movieRepository.findAll(example);
+        movies.forEach(each->{
+            log.info("{}", each);
+        });
+
+    }
+
+}
