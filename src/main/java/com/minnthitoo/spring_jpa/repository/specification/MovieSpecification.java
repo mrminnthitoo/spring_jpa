4a4e7984fae0a27a687cd09d5daf6ffe0eb55af1
mrminnthitoo <mrminnthitoo@gmail.com>
2024-11-21 22:15:00 +0630

    criteria query api

--- src/main/java/com/minnthitoo/spring_jpa/repository/specification/MovieSpecification.java
+++ src/main/java/com/minnthitoo/spring_jpa/repository/specification/MovieSpecification.java
@@ -1,15 +1,21 @@
 package com.minnthitoo.spring_jpa.repository.specification;
 
+import com.minnthitoo.spring_jpa.model.entity.Actor;
 import com.minnthitoo.spring_jpa.model.entity.Movie;
 import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MovieSpecification {
+
+    /*
+    * .greaterThan, .ge, .equal, greaterThanOrEqual ... are operators
+     */
+
     public static Specification<Movie> getMovieByYear(Long i){
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.<Long>get("year"), i);
     }
@@ -27,6 +33,43 @@ public class MovieSpecification {
             assert query != null;
             query.where(and);
             return and;
+        });
+    }
+
+    public static Specification<Movie> getMovieByYearGreaterThan(Long i){
+        return (root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.<Long>get("year"), i);
+    }
+
+    public static Specification<Movie> getMovieByYearBetween(Long start, Long end){
+        return (root, query, criteriaBuilder) -> criteriaBuilder.between(root.<Long>get("year"), start, end);
+    }
+
+    public static Specification<Movie> getMovieByTitleLike(String title){
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), "%" + title + "%");
+    }
+
+    public static Specification<Movie> getMovieWhereActorIn(String actor){
+        return ((root, query, criteriaBuilder) -> {
+            Join<Movie, Actor> actors = root.join("actors");
+            return criteriaBuilder.equal(actors.get("fullName"), actor);
+        });
+    }
+
+    public static Specification<Movie> getMovieGroupByGenre(String genres){
+        return ((root, query, criteriaBuilder) -> {
+            assert query != null;
+            query.orderBy(criteriaBuilder.asc(root.get("genre")));
+            return query.getRestriction();
+        });
+    }
+
+    public static Specification<Movie> getMovieInGenre(List<String> genres){
+        return ((root, query, criteriaBuilder) -> {
+            CriteriaBuilder.In<String> inClause = criteriaBuilder.in(root.get("genre"));
+            for (String genre : genres){
+                inClause.value(genre);
+            }
+            return inClause;
         });
     }
 }
--- src/test/java/com/minnthitoo/spring_jpa/repository/specification/MovieSpecificationTest.java
+++ src/test/java/com/minnthitoo/spring_jpa/repository/specification/MovieSpecificationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -28,11 +29,69 @@ public class MovieSpecificationTest {
 
     @Transactional
     @Test
-    public void TestGetMovieSpecByTitleAndYear(){
+    public void testGetMovieSpecByTitleAndYear(){
         List<Movie> movies = this.movieRepository.findAll(MovieSpecification.getMovieByTitleOrYear(null, 2010L));
         for (Movie movie : movies){
             log.info("{}", movie);
         }
     }
+
+    @Transactional
+    @Test
+    public void testGetMovieSpecByYearGreaterThan(){
+        List<Movie> movies = this.movieRepository.findAll(MovieSpecification.getMovieByYearGreaterThan(2010L));
+        for (Movie movie : movies){
+            log.info("{}", movie);
+        }
+    }
+
+    @Transactional
+    @Test
+    public void testGetMovieSpecByYearBetween(){
+        List<Movie> movies = this.movieRepository.findAll(MovieSpecification.getMovieByYearBetween(2010L, 2020L));
+        for (Movie movie : movies){
+            log.info("{}", movie);
+        }
+    }
+
+    @Transactional
+    @Test
+    public void testGetMovieByTitleLike(){
+        List<Movie> movies = this.movieRepository.findAll(MovieSpecification.getMovieByTitleLike("Test"));
+        for (Movie movie : movies){
+            log.info("{}", movie);
+        }
+    }
+
+    @Transactional
+    @Test
+    public void testGetMovieWithActorIn(){
+        List<Movie> movies = this.movieRepository.findAll(MovieSpecification.getMovieWhereActorIn("Leonado Dicaprao"));
+        for (Movie movie : movies){
+            log.info("{}", movie);
+        }
+    }
+
+    @Transactional
+    @Test
+    public void testGetMovieSepcByGenre(){
+        List<Movie> movies = this.movieRepository.findAll(MovieSpecification.getMovieGroupByGenre("Drama"));
+        for (Movie movie : movies){
+            log.info("{}", movie);
+        }
+    }
+
+    @Transactional
+    @Test
+    public void testGetMovieSepcInGenres(){
+        List<String> genres = new ArrayList<>();
+        genres.add("Drama");
+        genres.add("Sci-Fi");
+        List<Movie> movies = this.movieRepository.findAll(MovieSpecification.getMovieInGenre(genres));
+        for (Movie movie : movies){
+            log.info("{}", movie);
+        }
+    }
+
 
 }
